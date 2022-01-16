2024f779f6cf82ca7b3b39932d36339d99b021d8
ashurani57 <kishuashu57@gmail.com>
2022-01-17 02:43:20 +0530

    demo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import List from "./components/List"
+interface IState{
+  people:{
+    name: String
+    age: Number
+    url?: String
+    note?:String
+  }[]
+}
 function App() {
+  const[people, setPeople] = useState<IState["people"]>([
+    {
+    name: "Kritika",
+    url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.facebook.com%2FAnime-Girl-429719194145690%2Fposts&psig=AOvVaw1Vof60k4U9TUXg0ef7Haxz&ust=1642449211916000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCPjSh6eGt_UCFQAAAAAdAAAAABAD",
+    age: 22,
+    note: "Accenture employee",
+}
+])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Invited people list</h1>
+      <List people={people} />
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,33 @@
+import React  from "react";
+
+interface IProps{
+    people:{
+      name: String
+      age: Number
+      url?: String
+      note?:String
+    }[]
+  }
+const List: React.FC<IProps> = ({people})=>{
+    const renderList = (): JSX.Element[] => {
+        return people.map(person => {
+            return (
+                <li className="List">
+                    <div className="List-header">
+                        <img className="List-img" src={person.url}/>
+                        <h2>{person.name}</h2>
+                    </div>
+                    <p>{person.age} years old</p>
+                    <p className="List-note">{person.note}</p>
+                </li>
+            )
+        })
+    }
+    return(
+        <ul>
+            {renderList()}
+
+        </ul>
+    )
+}
+export default List
